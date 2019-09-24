ed904a7d8b24d27ac63c6723737b786a6f7f7bdc
Dave Briccetti <daveb@davebsoft.com>
2019-09-23 18:34:35 -0700

    Allow drawing with two different colors

--- livespacedraw.py
+++ livespacedraw.py
@@ -1,14 +1,26 @@
 import cv2 as cv
 import numpy as np
 
-cap = cv.VideoCapture(0)
+YELLOW = (0, 255, 255)
+ORANGE = (0, 128, 255)
+
+YELLOW_MIN = (0, 240, 240)
+YELLOW_MAX = (200, 255, 255)
+ORANGE_MIN = (60, 80, 205)
+ORANGE_MAX = (130, 130, 255)
+
+
+cap = cv.VideoCapture(1)
 accumulator_frame = None
 while True:
-    ret, frame = cap.read()
+    ret, large_frame = cap.read()
+    frame = cv.resize(large_frame, (0, 0), fx=0.5, fy=0.5)
     fs = frame.shape
     flipped = cv.flip(frame, 1)
-    mask = cv.inRange(flipped, (0, 120, 120), (20, 255, 255))
-    masked_input = cv.bitwise_and(flipped, flipped, mask=mask)
+    mask_orange = cv.inRange(flipped, ORANGE_MIN, ORANGE_MAX)
+    mask_yellow = cv.inRange(flipped, YELLOW_MIN, YELLOW_MAX)
+    mask_all = cv.bitwise_or(mask_yellow, mask_orange)
+    masked_input = cv.bitwise_and(flipped, flipped, mask=mask_all)
     if accumulator_frame is None:
         accumulator_frame = np.zeros(fs, dtype=np.uint8)
 
@@ -16,18 +28,20 @@ while True:
     p.filterByColor = False
     p.filterByConvexity = False
     p.filterByArea = True
-    p.minArea = 200
+    p.minArea = 100
     detector = cv.SimpleBlobDetector_create(p)
-    key_points = detector.detect(mask)  # Blob detection here for future enhancements
 
-    if key_points:
-        pt = key_points[0].pt
-        if pt[0] < 100 and pt[1] < 100:
-            accumulator_frame[:, :, :] = 0
-        cv.circle(accumulator_frame, (round(pt[0]), round(pt[1])), 30, (0, 255, 255), -1)
+    for mask, color in ((mask_orange, ORANGE), (mask_yellow, YELLOW)):
+        key_points = detector.detect(mask)
+
+        if key_points:
+            pt = key_points[0].pt
+            if pt[0] < 100 and pt[1] < 100:
+                accumulator_frame[:, :, :] = 0
+            cv.circle(accumulator_frame, (round(pt[0]), round(pt[1])), 5, color, -1)
 
     cv.imshow('Input', flipped)
-    cv.imshow('Yellow', mask)
+    cv.imshow('Masked', masked_input)
     cv.imshow('Time-Lapse', accumulator_frame)
     if cv.waitKey(1) == ord('q'):
         break
